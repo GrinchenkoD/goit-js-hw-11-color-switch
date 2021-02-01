afca4567cf3c0d23954621a720a9d0e3bb233105
Dmitry Grinchenko <ra1d3r@i.ua>
2021-02-01 09:10:04 +0200

    color-switch-done

---
+++ index.js
@@ -0,0 +1,34 @@
+// ⚠️ Учти, на кнопку Start можно нажать бесконечное количество раз.
+// Сделай так, чтобы пока изменение темы запушено, кнопка Start была не активна.
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const startRef = document.querySelector('[data-action="start"]');
+const stopRef = document.querySelector('[data-action="stop"]');
+let intervalId = null;
+
+const changeColor = function () {
+  if (intervalId) return;
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = `${
+      colors[randomIntegerFromInterval(0, 5)]
+    }`;
+  }, 1000);
+};
+
+const stopChange = function () {
+  clearInterval(intervalId);
+  intervalId = null;
+};
+
+startRef.addEventListener('click', changeColor);
+stopRef.addEventListener('click', stopChange);
